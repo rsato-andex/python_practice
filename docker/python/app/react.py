dff8f0a94ee4c947f155d8dff8235c3574da2f37
rsato-andex <r.sato@and-ex.co.jp>
2024-06-04 17:57:08 +0900

    react修正中

--- docker/python/app/react.py
+++ docker/python/app/react.py
@@ -2,10 +2,6 @@ from flask import Blueprint, send_from_directory, session, jsonify
 
 react = Blueprint('react', __name__, static_folder='static/build')
 
-@react.route('/')
-def serve():
-    return send_from_directory(react.static_folder, 'index.html')
-
 @react.route('/<path:path>')
 def static_proxy(path):
     return send_from_directory(react.static_folder, path)
